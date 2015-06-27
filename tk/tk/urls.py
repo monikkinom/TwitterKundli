c9c7c2d607468fb1186696dc8247a4d87e77fd16
monik <monik@bitbucket.org>
2015-06-27 12:58:29 +0530

    third commit

--- tk/tk/urls.py
+++ tk/tk/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from kundli import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 
 
 )
